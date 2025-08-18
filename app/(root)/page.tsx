4a7fb3a2a3d4c17aefbe80a12ee30b6c46862b94
Mohammad AlZarieni <mohammadzarieni@gmail.com>
2025-08-17 23:57:24 -0500

    Add interview card and tech icon display components
    Introduces InterviewCard and DisplayTechIcons components for rendering interview details and technology icons. Updates the root layout and homepage to use these components, refactors constants to be exported, and enhances utility functions for tech logo fetching and random interview cover selection. Adds dayjs dependency for date formatting.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,45 @@
+import React from 'react'
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
+import {dummyInterviews} from "@/constants";
+import InterviewCard from "@/components/InterviewCard";
 
-import { ReactNode } from 'react'
-
-const AuthLayout = ({ children }:{children:ReactNode} ) => {
+const Page = () => {
     return (
-        <div>{children}</div>
+        <>
+            <section className="card-cta">
+                <div className="flex flex-col gap-6 max-w-lg">
+                    <h2>Get Interview Ready with AI powered practice.</h2>
+
+                <p className="text-lg">
+                    Practice with AI powered questions & feedback.
+                </p>
+                <Button asChild className="btn-primary max-sm:w-full">
+                    <Link href="/interview">Start an Interview</Link>
+                </Button>
+                </div>
+                <Image src="/robot.png" alt="rob-dude" width={400} height={400} className="max-sm:hidden" />
+            </section>
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Your Interview</h2>
+                <div className="interviews-section">
+                    {dummyInterviews.map((interview) => (
+                        <InterviewCard {...interview} key={interview.id} />
+                    ))}
+
+                    {/*<p>You have not taken any Interviews yet.</p>*/}
+                </div>
+            </section>
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Take an Interview</h2>
+                <div className="interviews-section">
+                    {dummyInterviews.map((interview) => (
+                        <InterviewCard {...interview} key={interview.id}/>
+                    ))}
+                </div>
+            </section>
+        </>
     )
 }
-export default AuthLayout
+export default Page
